3f8a056c41f0196b7e53ab70da81f6f450bbb47e
Rowaa <Rowaaata67@gmail.com>
2022-06-03 18:57:17 +0200

    result

---
+++ lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String answerText;
+  final Function function;
+
+  Answer(this.function, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+
+        child: Text(answerText,style: TextStyle(fontSize:25 )),
+        onPressed: (){},
+      ),
+    );
+  }
+}
