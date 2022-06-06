67975c6ed0447d3675540dcd066a852d836e3c50
Rowaa <Rowaaata67@gmail.com>
2022-06-06 17:14:56 +0200

    result

--- lib/answer.dart
+++ lib/answer.dart
@@ -19,25 +19,3 @@ class Answer extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-
-class Answer extends StatelessWidget {
-  final String answerText;
-  final Function function;
-
-  Answer(this.function, this.answerText);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: RaisedButton(
-
-        child: Text(answerText,style: TextStyle(fontSize:25 )),
-        onPressed: (){},
-      ),
-    );
-  }
-}
->>>>>>> origin/main
--- lib/question.dart
+++ lib/question.dart
@@ -19,25 +19,3 @@ class Question extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-
-class Question extends StatelessWidget {
-  final String questionText;
-
-  Question(this.questionText);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(10.0),
-      child: Text(
-        questionText,
-        style: TextStyle(fontSize: 30),
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
-}
->>>>>>> origin/main
